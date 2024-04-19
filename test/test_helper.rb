2d002850f867aa159f6cbd69d51fe9c7d82461c3
navanit patil <sonawanenavanit1@gmail.com>
2024-04-19 11:14:22 +0000

    basic login implementation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,8 @@ module ActiveSupport
     fixtures :all
     include ApplicationHelper
 
-    # Add more helper methods to be used by all tests here...
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
