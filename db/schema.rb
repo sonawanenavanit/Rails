cd341cc46dc938dba327bbb7754f575e7590728b
navanit patil <sonawanenavanit1@gmail.com>
2024-04-23 10:10:42 +0000

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_084903) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_115735) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
